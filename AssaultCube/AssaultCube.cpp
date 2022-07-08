cd60eb611e50a8619371b7b5266b6df2fe9b25cf
Jesus-QC <jesusquirocampos@gmail.com>
2022-07-08 17:16:10 +0200

    Example external

---
+++ AssaultCube/AssaultCube.cpp
@@ -0,0 +1,52 @@
+﻿#include "AssaultCube.h"
+
+void input(HANDLE processHandle, uintptr_t player_address)
+{
+    while(true)
+    {
+        if (GetAsyncKeyState(VK_NUMPAD0)) // Easy example using the PositionZ offset to Lock the Z Axis.  (Flying)
+        {
+            float savedY;
+            ReadProcessMemory(processHandle, (BYTE*)(player_address + offsets::player_pos_z_offset), &savedY, sizeof(float), nullptr);
+            std::cout << "e";
+            Sleep(500);
+            while (true)
+            {
+                if(GetAsyncKeyState(VK_NUMPAD0))
+                    break;
+                
+                WriteProcessMemory(processHandle, (BYTE*)(player_address + offsets::player_pos_z_offset), &savedY, sizeof(float), nullptr);
+            }
+            
+            Sleep(100);
+        }
+        Sleep(10);
+    }
+}
+
+int main()
+{
+    HWND game_window = FindWindowA(nullptr, "AssaultCube");
+
+    if(game_window == nullptr)
+    {
+        std::cout << "Game Window not found!";
+        Sleep(3000);
+        return -1;
+    }
+    
+    DWORD processId;
+    GetWindowThreadProcessId(game_window, &processId);
+
+    std::cout << "Found process id: " << processId << std::endl;
+
+    HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, false, processId);
+
+    uintptr_t player_entry;
+    ReadProcessMemory(processHandle, (BYTE*)(offsets::game_module_address + offsets::game_module_offset), &player_entry, sizeof(player_entry), nullptr);
+
+    uintptr_t player_address = player_entry + offsets::player_offset;
+    std::cout << "Player address found: 0x" << std::hex << player_address << std::endl;
+    
+    input(processHandle, player_address);
+}
---
+++ AssaultCube/AssaultCube.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include <Windows.h>
+#include <iostream>
+
+namespace offsets
+{
+    inline uintptr_t game_module_address = 0x00400000;
+    inline uintptr_t game_module_offset = 0x0017E0A8;
+
+    inline uintptr_t player_offset = 0x28;
+
+    inline uintptr_t player_rot_x_offset = 0xC;
+    inline uintptr_t player_rot_y_offset = 0x10;
+
+    inline uintptr_t player_pos_x_offset = 0x0;
+    inline uintptr_t player_pos_y_offset = 0x4;
+    inline uintptr_t player_pos_z_offset = 0x8;
+
+    inline uintptr_t player_grenades_offset = 0x11C;
+    inline uintptr_t player_health_offset = 0xC4;
+    inline uintptr_t player_shield_offset = 0xC8;
+}
